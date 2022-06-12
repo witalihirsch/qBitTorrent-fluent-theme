da4fd8f35761e67aff464c186078ae422d8f94cf
witalihirsch <witalihirsch1@gmail.com>
2022-06-12 21:03:39 +0700

    release v2.2

---
+++ Builds/build-fluent-dark-no-mica.bat
@@ -0,0 +1,2 @@
+set SCRIPT_DIR=fluent-theme
+python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir icons/dark -output fluent-dark-no-mica -style darknomica.qss 
